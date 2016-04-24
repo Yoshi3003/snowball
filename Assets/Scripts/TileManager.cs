062ef879c4c04f4249ceed0826e0e69b55552f67
jitrapon <jitrapon@treeboxsolutions.com>
2016-04-24 19:09:47 +0800

    Spawn tile when ball exits tile

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileManager : MonoBehaviour {
+
+	public GameObject currentTile;
+	public List<GameObject> spawnTiles;
+
+	private static TileManager instance;
+
+	public static TileManager Instance {
+		get { 
+			if (instance == null)
+				instance = GameObject.FindObjectOfType<TileManager> ();
+			return TileManager.instance; 
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	GameObject GetNextTile() {
+		return spawnTiles [Random.Range (0, spawnTiles.Count)];
+	}
+
+	public void SpawnTile() {
+		currentTile = (GameObject) Instantiate (GetNextTile(), 
+			currentTile.transform.Find ("AttachPoint").position, Quaternion.identity);
+	}
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionExit(Collision collision) {
+		if (collision.gameObject.tag == "Player") {
+			TileManager.Instance.SpawnTile ();
+		}
+	}
+}
